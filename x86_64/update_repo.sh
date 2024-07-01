de96f2614e9148999e535e3361c66878a0f8fc0a
Biaogo <481622717@qq.com>
2024-07-01 19:24:52 +0800

    update

--- x86_64/update_repo.sh
+++ x86_64/update_repo.sh
@@ -2,7 +2,7 @@
 
 REPO_NAME="neko"
 
-rm *.db *.db.sig *.db.tar.gz *.db.tar.gz.sig *.files *.files.sig *.files.tar.gz *.files.tar.gz.sig *.pkg.tar.zst.sig
+rm -f *.db *.db.sig *.db.tar.gz *.db.tar.gz.sig *.files *.files.sig *.files.tar.gz *.files.tar.gz.sig *.pkg.tar.zst.sig
 
 repo-add --verify --sign -R $REPO_NAME.db.tar.gz *.pkg.tar.zst
 
