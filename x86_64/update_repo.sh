2cbc9aa745be8e138a5f624ad7471d8b78df4ea4
Biaogo <481622717@qq.com>
2024-07-01 18:50:47 +0800

    Add built packages

--- x86_64/update_repo.sh
+++ x86_64/update_repo.sh
@@ -6,6 +6,13 @@ rm *.db *.db.sig *.db.tar.gz *.db.tar.gz.sig *.files *.files.sig *.files.tar.gz 
 
 repo-add --verify --sign -R $REPO_NAME.db.tar.gz *.pkg.tar.zst
 
+rm $REPO_NAME.db $REPO_NAME.db.sig $REPO_NAME.files $REPO_NAME.files.sig
+
+cp $REPO_NAME.db.tar.gz $REPO_NAME.db
+cp $REPO_NAME.db.tar.gz.sig $REPO_NAME.db.sig
+cp $REPO_NAME.files.tar.gz $REPO_NAME.files
+cp $REPO_NAME.files.tar.gz.sig $REPO_NAME.files.sig
+
 ./sign_packages.sh
 
 echo "####################################"
